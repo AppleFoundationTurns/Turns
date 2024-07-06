2da6263a66af2ada563c1379c5e3263a05e6834c
Gheim2 <peppedamiata1@gmail.com>
2024-07-06 20:05:40 +0200

    Added working buttons to the scene with animation on press. Added animations also for jump, run and for both direction. Added 2 enums in Utilities file to handle the actual action and direction of the hero. Splitted FlameNode from HeroNode. Moved anchorPoint for the player (and the flame) to the feet.

---
+++ Turns-v1/Custom Classes/FlameNode.swift
@@ -0,0 +1,8 @@
+//
+//  FlameNode.swift
+//  Turns-v1
+//
+//  Created by Giuseppe Damiata on 06/07/24.
+//
+
+import Foundation
---
+++ Turns-v1/Custom Classes/utilities.swift
@@ -0,0 +1,8 @@
+//
+//  utilities.swift
+//  Turns-v1
+//
+//  Created by Giuseppe Damiata on 06/07/24.
+//
+
+import Foundation
