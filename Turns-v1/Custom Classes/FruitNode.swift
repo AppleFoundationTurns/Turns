06cb22230d48e2c21abcb51bfd689716e61a42d7
Gheim2 <peppedamiata1@gmail.com>
2024-07-10 11:25:03 +0200

    Added FruitNode to handle fruit collection

--- Turns-v1/Custom Classes/FruitNode.swift
+++ Turns-v1/Custom Classes/FruitNode.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import SpriteKit
+
+class FruitNode : SKSpriteNode {
+    var collected = false
+    
+    func animate() {
+        let scale = SKAction.scale(by: 1.5, duration: 0.2)
+        let fadeOut = SKAction.fadeOut(withDuration: 0.2)
+        let sound = SKAction.playSoundFileNamed("", waitForCompletion: false)
+        let collectSequence = SKAction.sequence([scale, fadeOut])
+    }
+}
